a8b5df794ba190c5ecc65b9b9449ed74476dd965
Priyansh Thakar <priyanshthakar@gmail.com>
2023-05-15 11:20:39 -0400

    Added Ids to View in Scene Builder and copied controller content

--- src/main/java/com/example/week2/CreateBookController.java
+++ src/main/java/com/example/week2/CreateBookController.java
@@ -1,4 +1,38 @@
 package com.example.week2;
 
-public class CreateBookController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CreateBookController implements Initializable {
+
+    @FXML
+    private TextField authorTextField;
+
+    @FXML
+    private CheckBox availibilityCheckBox;
+
+    @FXML
+    private TextField bookNameTextField;
+
+    @FXML
+    private Label finalLabel;
+
+    @FXML
+    private ComboBox<String> genreComboBox;
+
+    @FXML
+    private Spinner<Double> priceSpinner;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        finalLabel.setVisible(false);
+    }
 }
